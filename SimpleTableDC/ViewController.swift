e9a3d6d0bc5afd796294d191e31e63c8b11f9062
danielCatlett <daniel.catlett42@gmail.com>
2017-03-04 23:28:17 -0600

    Might as well upload

--- SimpleTableDC/ViewController.swift
+++ SimpleTableDC/ViewController.swift
@@ -1,25 +1,80 @@
-//
 //  ViewController.swift
 //  SimpleTableDC
 //
 //  Created by Daniel Catlett on 3/4/17.
 //  Copyright © 2017 Daniel Catlett. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    private let dwarves = ["Sleepy", "Sneezy", "Bashful", "Happy", "Doc", "Grumpy", "Dopey", "Thorin", "Dorin", "Nori", "Ori", "Balin", "Dwalin", "Fili", "Kili", "Oin", "Gloin", "Bifur", "Bofur", "Bombur"]
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK:-
+    //MARK: Table View Data Source Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return dwarves.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if(cell == nil)
+        {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: simpleTableIdentifier)
+        }
+        
+        let image = UIImage(named: "star")
+        cell?.imageView?.image = image
+        let highlightedImage = UIImage(named: "star2")
+        cell?.imageView?.highlightedImage = highlightedImage
+        
+        if(indexPath.row < 7)
+        {
+            cell?.detailTextLabel?.text = "Mr. Disney"
+        }
+        else
+        {
+            cell?.detailTextLabel?.text = "Mr.Tolkien"
+        }
+        
+        cell?.textLabel?.text = dwarves[indexPath.row]
+        
+        return cell!
+    }
+    
+    //MARK:-
+    //MARK: Table View Delegate Methods
+    func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int
+    {
+        return indexPath.row % 4
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath?
+    {
+        return indexPath.row == 0 ? nil : indexPath
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        let rowValue = dwarves[indexPath.row]
+        let message = "You selected \(rowValue)"
+        
+        let controller = UIAlertController(title: "Row Selected", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yes I Did", style: .default, handler: nil)
+        controller.addAction(action)
+        present(controller, animated: true, completion: nil)
+    }
 }
 
